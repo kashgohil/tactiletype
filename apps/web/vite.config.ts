656f54b3be97947dbadee1189eec9bb4e3e7d699
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-28 00:04:10 +0530

    [chore] vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3002,
+    proxy: {
+      '/api': { changeOrigin: true, target: 'http://localhost:8002' },
+    },
+  },
+  plugins: [react(), tanstackRouter(), tailwindcss()],
+});
