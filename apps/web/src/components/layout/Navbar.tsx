8c0e0855de3365b6f58fcae9dbc3e067ea8bfebc
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-29 19:43:43 +0530

    [chore] added multiplayer link

--- apps/web/src/components/layout/Navbar.tsx
+++ apps/web/src/components/layout/Navbar.tsx
@@ -28,6 +28,13 @@ export const Navbar: React.FC = () => {
               Leaderboard
             </Link>
 
+            <Link
+              to="/multiplayer"
+              className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
+            >
+              Multiplayer
+            </Link>
+
             {user ? (
               <div className="flex items-center space-x-4">
                 <Link
