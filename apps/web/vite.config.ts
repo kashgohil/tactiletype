43d8946c1454047e68562954f289985976ddb705
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-08-26 22:23:47 +0530

    [shadcn] setting up shadcn components

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -19,4 +20,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
