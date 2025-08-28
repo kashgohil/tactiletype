63e7aab94696484dd0628a7cabd3ef7c65781dc0
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-08-28 16:26:35 +0530

    [components] stopwatch component

---
+++ apps/web/src/components/stopwatch.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useMemo, useState } from 'react';
+
+export function Stopwatch({
+  duration,
+  onEnd,
+}: {
+  duration: number;
+  onEnd: () => void;
+}) {
+  const [timeLeft, setTimeLeft] = useState(duration);
+
+  const { hours, minutes, seconds } = useMemo(() => {
+    const hours = Math.floor(timeLeft / 3600);
+    const mins = Math.floor((timeLeft % 3600) / 60);
+    const secs = timeLeft % 60;
+    return {
+      hours: hours.toString().padStart(2, '0'),
+      minutes: mins.toString().padStart(2, '0'),
+      seconds: secs.toString().padStart(2, '0'),
+    };
+  }, [timeLeft]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (!timeLeft) {
+          clearInterval(intervalId);
+          onEnd();
+          return 0;
+        }
+        return timeLeft - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [onEnd]);
+
+  return (
+    <span className="flex items-center">{`${hours}:${minutes}:${seconds}`}</span>
+  );
+}
