e36889b7f21e2685e2498fde7bc85d39f85f6d72
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-09-04 16:24:55 +0530

    [fix] fix the db url

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,14 @@
-import { DATABASE_URL } from '@/apps/api/src/constants';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const client = postgres(DATABASE_URL, { max: 1 });
+const connectionString =
+  process.env.DATABASE_URL || 'postgresql://localhost:5432/tactile';
+const client = postgres(connectionString, { max: 1 });
 
 export const db = drizzle(client, { schema });
 export * from './schema';
+
 export type Database = typeof db;
 
 // Migration utilities
